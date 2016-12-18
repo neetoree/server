e423059bceb569b76e77d6f13ca95594ce6f787d
Alexander Tumin <iamtakingiteasy@eientei.org>
2016-12-18 06:24:52 +0300

    Recaptcha

--- src/main/java/org/neetoree/server/UserController.java
+++ src/main/java/org/neetoree/server/UserController.java
@@ -5,11 +5,13 @@ import net.tanesha.recaptcha.ReCaptchaFactory;
 import net.tanesha.recaptcha.ReCaptchaResponse;
 import org.neetoree.server.orm.UserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
 
@@ -22,12 +24,23 @@ public class UserController {
     private final TokenStore tokenStore;
     private final UserRepository userRepository;
 
-    private ReCaptcha reCaptcha = ReCaptchaFactory.newReCaptcha(System.getProperty("re.pub"), System.getProperty("re.priv"), false);
+    @Value("recaptcha.pub")
+    private String pubkey;
+
+    @Value("recaptcha.priv")
+    private String privkey;
+
+    private ReCaptcha reCaptcha;
 
     @Autowired
     public UserController(TokenStore tokenStore, UserRepository userRepository) {
         this.tokenStore = tokenStore;
         this.userRepository = userRepository;
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        reCaptcha = ReCaptchaFactory.newReCaptcha(pubkey, privkey, false)
     }
 
     @RequestMapping("check/{token}")
@@ -44,15 +57,20 @@ public class UserController {
     @ResponseBody
     public boolean signup(@RequestBody UserSignupForm form, HttpServletRequest request) {
         String header = request.getHeader("X-Real-IP");
-        ReCaptchaResponse reCaptchaResponse = reCaptcha.checkAnswer(header, form.getChallenge(), form.getUresponse());
-        if (reCaptchaResponse.isValid()) {
-            UserEntity userEntity = new UserEntity();
-            userEntity.setUsername(form.getUsername());
-            userEntity.setPassword(form.getPassword());
-            userEntity.setCreated(new Date());
-            userRepository.save(userEntity);
-            return true;
+        if (form.getUsername().length() < 3 || form.getUsername().length() > 20 || !form.getUsername().matches("[a-zA-Z0-9]+")) {
+            return false;
         }
-        return false;
+
+        ReCaptchaResponse reCaptchaResponse = reCaptcha.checkAnswer(header, form.getChallenge(), form.getUresponse());
+        if (!reCaptchaResponse.isValid()) {
+            return false;
+        }
+
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUsername(form.getUsername());
+        userEntity.setPassword(form.getPassword());
+        userEntity.setCreated(new Date());
+        userRepository.save(userEntity);
+        return true;
     }
 }
